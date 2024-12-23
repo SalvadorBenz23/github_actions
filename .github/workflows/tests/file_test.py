1d399220d3d1c758a4783d1482bf20d81458ea33
SalvadorBenz23 <117619181+SalvadorBenz23@users.noreply.github.com>
2024-12-23 21:08:24 +0100

    Fix PEP8 issues and clean up unused imports

--- .github/workflows/tests/file_test.py
+++ .github/workflows/tests/file_test.py
@@ -1,15 +1,23 @@
+
 def test_calc_addition():
+    # Function test the output of 2+4
     output = 2 + 4
     assert output == 6
 
-def test_calc_subtraction():
+
+def test_calc_substraction():
+    # Function test the output of 2-4
     output = 2 - 4
     assert output == -2
 
+
 def test_calc_multiply():
+    # Function test the output of 2*4
     output = 2 * 4
     assert output == 8
 
-def test_hello():
-    output = "hello"
-    assert output == "hello"
+
+def test_coucou():
+    # Function test if the output returns 'hello'
+    output = 'hello'
+    assert output == 'hello'
